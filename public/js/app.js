a67c017fbd1381b67b734b2d195af0d894488bd0
Anthony Ku <anthony.yc.ku@gmail.com>
2021-01-13 16:51:53 -0500

    added to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,42 @@
+class Create extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+class Show extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+class Edit extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
 
 class App extends React.Component {
     state = {
-
     }
     render = () => {
         return <div id="react-container">
             <h1>React ToDo</h1>
 
-            <Name />
+
         </div>
     }
 }
