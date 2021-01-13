8292e2e8023cd89740ba779b3a4a26978ceb0022
Anthony Ku <anthony.yc.ku@gmail.com>
2021-01-13 16:10:05 -0500

    updated schema

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,13 @@
+
 class App extends React.Component {
     state = {
-        
+
     }
     render = () => {
         return <div id="react-container">
             <h1>React ToDo</h1>
 
-
+            <Name />
         </div>
     }
 }
