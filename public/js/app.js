4e2b44f416253180aec892c8bedba742a5ed77d0
Your Name <you@example.com>
2021-01-13 13:46:56 -0700

    Initial front-end setup

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+class App extends React.Component {
+    state = {
+        
+    }
+    render = () => {
+        return <div id="react-container">
+            <h1>React ToDo</h1>
+
+
+        </div>
+    }
+}
+
+
+ReactDOM.render(<App></App>, document.querySelector('.body-container'))
